e49a396402fda8b7df6b784d19fd388adc5cc01c
christinekarimi <karimichristine@ymail.com>
2018-03-28 14:28:40 +0300

    Adda functionality to test if a contact object exists

--- contact.py
+++ contact.py
@@ -33,8 +33,8 @@ def delete_contact(self):
 
         Contact.contact_list.remove(self)
 
-        @classmethod
-        
+@classmethod
+
 def find_by_number(cls,number):
         '''
         Method that takes in a number and returns a contact that matches that number.
@@ -48,3 +48,17 @@ def find_by_number(cls,number):
         for contact in cls.contact_list:
             if contact.phone_number == number:
                 return contact
+
+def contact_exist(cls,number):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            number: Phone number to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                    return True
+
+        return False
--- contact_test.py
+++ contact_test.py
@@ -71,7 +71,8 @@ def test_save_multiple_contact(self):
         '''
             test_save_multiple_contact to check if we can save multiple contact
             objects to our contact_list
-            '''
+
+        '''
             self.new_contact.save_contact()
             test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
             test_contact.save_contact()
@@ -112,6 +113,22 @@ def test_save_multiple_contact(self):
         self.assertEqual(found_contact.email,test_contact.email)
                    
 
+#--------------------------------------------------------------------------------------------------------------------------------
+
+#Test 6
+
+def test_contact_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the contact.
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
+        test_contact.save_contact()
+
+        contact_exists = Contact.contact_exist("0711223344")
+
+        self.assertTrue(contact_exists)
 
 
 
