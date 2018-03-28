e927196207e4e3c9336f4eea8ab6893d60c9a7b0
christinekarimi <karimichristine@ymail.com>
2018-03-28 14:19:00 +0300

    Added test 5

--- contact.py
+++ contact.py
@@ -32,3 +32,19 @@ def delete_contact(self):
         '''
 
         Contact.contact_list.remove(self)
+
+        @classmethod
+        
+def find_by_number(cls,number):
+        '''
+        Method that takes in a number and returns a contact that matches that number.
+
+        Args:
+            number: Phone number to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.phone_number == number:
+                return contact
