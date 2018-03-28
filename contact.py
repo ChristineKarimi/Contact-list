d06f0e6d34185955a3eb7facd24679a5a6c5734d
christinekarimi <karimichristine@ymail.com>
2018-03-28 13:07:19 +0300

    Initial commit

---
+++ contact.py
@@ -0,0 +1,26 @@
+class Contact:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    contact_list = [] # Empty contact list
+
+    def __init__(self,first_name,last_name,phone_number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+
+contact_list = [] # Empty contact list
+ # Init method up here
+  
+def save_contact(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        Contact.contact_list.append(self)
